d1896f058cbcad159905f1ebb72e36e1fd4d856b
vamsi943 <143083060+vamsi943@users.noreply.github.com>
2024-08-07 21:59:32 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+  
+            const targetElement = document.querySelector(this.getAttribute('href'));
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+  });
